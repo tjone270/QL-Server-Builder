f76922c209ea27676115d8aeffc7ed982aba7d72
Thomas Jones <thomas@tomtecsolutions.com>
2016-07-01 13:13:28 +1000

    added some js for the instagib checkbox

--- assets/javascript/global.js
+++ assets/javascript/global.js
@@ -2,13 +2,41 @@
 /* Global JavaScript scripting file */
 
 
-function enableWeaponCheckboxes() {
-  var checkboxes = document.getElementsByClassName("weapon_checkbox");
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].disabled) {
-      checkboxes[i].disabled = false;
+function toggleWeaponCheckboxes() {
+  var element = document.getElementsByClassName("weapon_checkbox");
+  for (var i = 0; i < element.length; i++) {
+    if (element[i].disabled) {
+      element[i].disabled = false;
     } else {
-      checkboxes[i].disabled = true;
+      element[i].disabled = true;
+    }
+  }
+}
+
+function toggleInstagib() {
+  var element = document.getElementsByClassName("infiniteammo-radio");
+  var weapon_checkboxes = document.getElementsByClassName("weapon_checkbox");
+  for (var i = 0; i < element.length; i++) {
+    if (element[i].disabled) {
+      element[i].disabled = false;
+      document.getElementById("defaultWeaponsCheckbox").disabled = false;
+      if (!document.getElementById("defaultWeaponsCheckbox").checked) {
+        for (var j = 0; j < weapon_checkboxes.length; j++) {
+          weapon_checkboxes[j].disabled = false;
+        }
+      } else {
+        for (var j = 0; j < weapon_checkboxes.length; j++) {
+          weapon_checkboxes[j].disabled = true;
+        }
+      }
+      document.getElementById("instaGibMessage").innerHTML = "";
+    } else {
+      element[i].disabled = true;
+      document.getElementById("defaultWeaponsCheckbox").disabled = true;
+      for (var k = 0; k < weapon_checkboxes.length; k++) {
+        weapon_checkboxes[k].disabled = true;
+      }
+      document.getElementById("instaGibMessage").innerHTML = "Weapon selection has been overridden to Gauntlet and Railgun while Insta-Gib is enabled.<br />Infinite ammo has been overridden to always on while Insta-Gib is enabled.";
     }
   }
 }
@@ -16,7 +44,7 @@ function enableWeaponCheckboxes() {
 function updateFactory() {
   factory_name = document.getElementById("server_factory");
   factory_name = factory_name.options[factory_name.selectedIndex].text;
-  checkboxHtml = "<input type=\"checkbox\" onclick=\"enableWeaponCheckboxes()\" name=\"g_startingWeapons[]\" value=\"DEFAULT\" checked>" // this is _REALLY_ shitty...
+  checkboxHtml = "<input type=\"checkbox\" id=\"defaultWeaponsCheckbox\" onclick=\"toggleWeaponCheckboxes()\" name=\"g_startingWeapons[]\" value=\"DEFAULT\" checked>" // this is _REALLY_ shitty...
   document.getElementById("defaultWeaponsCheckboxLabel").innerHTML = checkboxHtml + "Default for " + factory_name;
 }
 
